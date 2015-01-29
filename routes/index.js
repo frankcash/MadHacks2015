cd04531c4cba80dff525dca5ace328f931c00d8a
Joseph Dailey <josephldailey@gmail.com>
2015-01-28 21:16:51 -0800

    applications

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Madhacks' });
+	res.render('index', { title: 'Madhacks' });
 });
 router.get('/email', function(req, res) {
-  res.render('email_subscribe', { title: 'Madhacks', rowid:0 });
+	res.render('email_subscribe', { title: 'Madhacks', rowid:0 });
 });
 
 router.get('/lightshow', function(req, res) {
-  res.render('lightning', { title: 'Madhacks' });
+	res.render('lightning', { title: 'Madhacks' });
 });
 
+router.get('/apply', function(req, res) {
+	res.render('application', {tittle: 'Apply'});
+});
 
 module.exports = router;
